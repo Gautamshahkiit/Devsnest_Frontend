9759b01a3273ad59f6d3a853d337e93e81c7bc3e
Gautamshahkiit <2029165@kiit.ac.in>
2021-07-14 12:56:30 +0530

    day 10

---
+++ Day_10/readme.md
@@ -0,0 +1,15 @@
+# Memory game
+
+### Steps: 
+- first create 20 boxes at the centre of the screen
+- create 10 names for a pair of boxes.
+- randomly allocate each names to a box (must be two boxes with same name)
+- add event listener of click that flips the box
+- let another card be flipped too & if both are same, make their visibility hidden else unflip both the boxes.
+- if all the cards are hidden then alert the user that they have won.
+
+### Aditional Features: 
+- keep the track of steps used.
+- keep a timer that bounds the user to complete their game in that time bound or else they lose.
+- keep track of the high score too.
+- rate the user via 1/2/3 stars
---
+++ Day_10/script.js
@@ -0,0 +1,47 @@
+console.log("hello day 10");
+// const inner = document.querySelector(".outer");
+// for (let i = 0; i < 20; i++) {
+//   inner.innerHTML += '<div class="box"></div>';
+// }
+
+let words = ["sharique", "emin", "amit", "akshansh", "prasanna", "shimanshu", "sumit", "monika", "popoye", "gauti"];
+words = [...words, ...words];
+console.log(words);
+
+function randomize ()  {
+  return Math.floor(Math.random() * (words.length));
+};
+console.log(randomize());
+
+let moves  = 0;
+let matches = 0;
+const n = words.length;
+
+for (let i = 1; i <= n; i++) {
+  const card = document.createElement("div");
+  card.classList.add("flip-inner");
+  
+  const inner = document.createElement("div");
+  inner.classList.add("flip-inner");
+
+  const front = document.createElement("div");
+  front.classList.add("front");
+
+  const back = document.createElement("div");
+  const rand = randomize();
+  back.innerHTML = "<h2>" + words[rand] + "</h2>";
+  words.splice(rand, 1);
+  // console.log(words);
+
+  back.classList.add("back");
+  inner.appendChild(front);
+  inner.appendChild(back);
+  card.appendChild(inner);
+  document.querySelector(".grid").appendChild(card);
+}
+
+// const boxes = document.querySelectorAll(".box");
+// boxes.forEach((box) => {
+//   box.addEventListener("click", () => {
+//   })
+// })
