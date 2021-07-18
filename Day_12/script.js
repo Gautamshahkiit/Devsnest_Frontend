5bdcb99925b2d49a8f22be11ec0933535a11383b
Gautamshahkiit <2029165@kiit.ac.in>
2021-07-18 23:27:38 +0530

    day 12

---
+++ Day_12/script.js
@@ -0,0 +1,19 @@
+console.log("hello day 12");
+
+let addToDoButton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById('inputField');
+
+addToDoButton.addEventListener("click", () => {
+  var paragraph = document.createElement('p');
+  paragraph.classList.add('paragraph-styling');
+  paragraph.innerText = inputField.value;
+  toDoContainer.appendChild(paragraph);
+  inputField.value = "";
+  paragraph.addEventListener("click", () => {
+    paragraph.style.textDecoration = "line-through";
+  })
+  paragraph.addEventListener("dblclick", () => {
+    toDoContainer.removeChild(paragraph);
+  });
+});
