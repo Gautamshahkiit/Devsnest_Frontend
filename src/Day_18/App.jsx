def5d5615e43e989ddf29ec812f365de60298f5d
Gautamshahkiit <2029165@kiit.ac.in>
2021-07-28 21:28:08 +0530

    day 19

---
+++ src/Day_18/App.jsx
@@ -0,0 +1,13 @@
+import Calorie from './calorieApp/CardContainer'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="app">
+      <h1 className="header">Calorie Meter</h1>
+      <Calorie />
+    </div>
+  );
+}
+
+export default App
